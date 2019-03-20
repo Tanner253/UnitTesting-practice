a139fcf42b7e15807365cd88a0a292eaec0664f8
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-03-20 10:43:34 -0700

    adds comments and cleans up code a little

--- lab02/lab02/Program.cs
+++ lab02/lab02/Program.cs
@@ -72,22 +72,41 @@ namespace lab02
             int choice = Convert.ToInt32(rawChoice);
             return choice;
         }
+        /// <summary>
+        /// will write the current ATM balance to program
+        /// </summary>
+        /// <returns>intiger value of balance</returns>
         public static int ViewBalance()
         {
            int viewBalance = currentBalance;
            return viewBalance;
         }
+        /// <summary>
+        /// adds funds to the existing current balance
+        /// </summary>
+        /// <param name="currentBalance">the current ATM balance</param>
+        /// <param name="input">The number equivilent to the funds the user would like to add</param>
+        /// <returns>new current balance</returns>
         public static int AddFunds(int currentBalance, int input)
         {
             
             currentBalance = currentBalance + input;
             return currentBalance;
         }
+        /// <summary>
+        /// will take current balance and subtract withdrawl amount
+        /// </summary>
+        /// <param name="currentValue">current balance</param>
+        /// <param name="withdrawl">the amount being subtracted from total</param>
+        /// <returns>the new balance after math operation</returns>
         public static int RemoveFunds(int currentValue, int withdrawl)
          {
             currentBalance = currentValue - withdrawl;
             return currentBalance;
         }
+        /// <summary>
+        /// allows the user to exit the program
+        /// </summary>
         public static void ExitProgram()
         {
             doWhileTrue = false;
