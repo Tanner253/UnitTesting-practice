422c912e4c9aad2f1199c6edbe4675f9b5e6f950
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-03-20 10:30:16 -0700

    finishes logic

--- lab02/XUnitTestProject1/UnitTest1.cs
+++ lab02/XUnitTestProject1/UnitTest1.cs
@@ -7,15 +7,39 @@ namespace XUnitTestlab02
     public class UnitTest1
     {
         [Fact]
-        public void CanReturnANumber()
+        public void CanReturnBalance()
         {
             //Arrange
             int param = 5;
             //Act
-            int output = ViewBalance(param);
+            int output = ViewBalance();
             //Assert
-            Assert.Equal(5, output);
+            Assert.Equal(5, param);
 
+        }
+        [Fact]
+        public void CanDeposit()
+        {
+            //Arrange
+            int num1 = 1;
+            int num2 = 6;
+
+            //Act
+            int output = AddFunds(num1, num2);
+            //Assert
+            Assert.Equal(7, output);
+
+        }
+        [Fact]
+        public void CanWithdraw()
+        {
+            //Arrange
+            int currentVal = 40;
+            int withdrawl = 20;
+            //Act
+            int output = RemoveFunds(currentVal, withdrawl);
+            //Assert
+            Assert.Equal(20, output);
         }
     }
 }
--- lab02/lab02/Program.cs
+++ lab02/lab02/Program.cs
@@ -5,7 +5,61 @@ namespace lab02
     public class Program
     {
         public static int startingBalance = 2000;
+        public static int currentBalance = startingBalance;
+        public static bool doWhileTrue = true;
         public static void Main(string[] args)
+        {
+            
+            
+            do 
+            {   int choice = GetSelection();
+                int caseSwitch = choice;
+                switch (caseSwitch)
+                {
+                    case 1:
+
+                        Console.WriteLine(ViewBalance());
+                        
+                       // GetSelection();
+                        
+                        break;
+                    case 2:
+                        Console.Write("How much money would you like to Withdraw?");
+                        string withdrawlAmount = Console.ReadLine();
+                        int intWithdrawl = Convert.ToInt32(withdrawlAmount);
+                        
+                        Console.WriteLine($"your new balance is {currentBalance} - {intWithdrawl} =");
+                        
+                        Console.WriteLine(RemoveFunds(currentBalance, intWithdrawl));
+                        
+                       // GetSelection();
+                        
+                        break;
+                    case 3:
+                        Console.Write("How much money would you like to Deposit?");
+                        string intInput = Console.ReadLine();
+                        int input = Convert.ToInt32(intInput);
+                        AddFunds(currentBalance, input);
+                        Console.WriteLine($"your new balance is {currentBalance} + {input} = ");
+                        currentBalance = currentBalance + input;
+                        Console.WriteLine($"{currentBalance}");
+                        
+                     //   GetSelection();
+                        
+                        break;
+                    case 4:
+                        ExitProgram();
+                        break;
+                }
+                
+
+            }
+            while (doWhileTrue) ;
+
+            
+        }
+        
+        public static int GetSelection()
         {
             string rawChoice;
             Console.WriteLine("Welcome to your virtual ATM:");
@@ -13,36 +67,30 @@ namespace lab02
             Console.WriteLine("2. Withdraw");
             Console.WriteLine("3. Deposit");
             Console.WriteLine("4. End");
-            
+
             rawChoice = Console.ReadLine();
             int choice = Convert.ToInt32(rawChoice);
-            int caseSwitch = choice;
-            switch(caseSwitch)
-            {
-                case 1:
-                    //do functions
-                    break;
-                case 2:
-                    //do functions
-                    break;
-                case 3:
-                    //do functions
-                    break;
-                case 4:
-                    //do functions
-                    break;
-            }
-
-            
+            return choice;
         }
-        public static int ViewBalance(int currentVal)
+        public static int ViewBalance()
         {
-           int currentBalance = currentVal;
-           return currentBalance;
+           int viewBalance = currentBalance;
+           return viewBalance;
         }
-
+        public static int AddFunds(int currentBalance, int input)
+        {
+            
+            currentBalance = currentBalance + input;
+            return currentBalance;
+        }
+        public static int RemoveFunds(int currentValue, int withdrawl)
+         {
+            currentBalance = currentValue - withdrawl;
+            return currentBalance;
+        }
         public static void ExitProgram()
         {
+            doWhileTrue = false;
             return;
         }
 
