13bdb4e61e322b14038c03f84de87d19cfbbb771
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-03-19 20:09:50 -0700

    scafolding for logic

---
+++ lab02/lab02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace lab02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rawChoice;
+            Console.WriteLine("Welcome to your virtual ATM:");
+            Console.WriteLine("1. View Balance");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Deposit");
+            Console.WriteLine("4. End");
+            rawChoice = Console.ReadLine();
+            int choice = Convert.ToInt32(rawChoice);
+            int caseSwitch = choice;
+            switch(caseSwitch)
+            {
+                case 1:
+                    //do functions
+                    break;
+                case 2:
+                    //do functions
+                    break;
+                case 3:
+                    //do functions
+                    break;
+                case 4:
+                    //do functions
+                    break;
+            }
+
+            
+        }
+    }
+}
