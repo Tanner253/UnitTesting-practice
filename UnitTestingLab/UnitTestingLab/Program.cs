f98fcdf129e997585df6ccd6b2d4ed324b100ef7
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-03-19 18:39:21 -0700

    adds some writeLines to main

--- UnitTestingLab/UnitTestingLab/Program.cs
+++ UnitTestingLab/UnitTestingLab/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections;
 
 namespace UnitTestingLab
 {
-    class Program
+    public class Program
     {
+        public static int currentBalance = 5000;
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int choice;
+            Console.WriteLine("Welcome to your virtual ATM:");
+            Console.WriteLine($"your balance is: {currentBalance}");
+            Console.WriteLine("1. Withdraw funds");
+            Console.WriteLine("2. Deposit funds");
+            Console.WriteLine("3. View balance");
+            Console.WriteLine("4. Exit");
+            string stringChoice = Console.ReadLine();
+            choice = Convert.ToInt32(stringChoice);
+            int caseSwitch = choice;
+            switch (caseSwitch)
+            {
+                case 1:
+                    //run function 1
+                    break;
+                case 2:
+                    //run func2
+                    break;
+                case 3:
+                    //run func 3
+                    break;
+            }
+
         }
-    }
+        }
+       
 }
